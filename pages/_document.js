7181775904b99f166e2e5af5db48d920dbd7416b
calmighted <manishh@tutanota.com>
2022-06-06 19:31:37 +0530

    crossorigin="true"

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,11 @@ class MyDocument extends Document {
 				<Head>
 					{/* FONTS */}
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
-					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+					<link
+						rel="preconnect"
+						href="https://fonts.gstatic.com"
+						crossOrigin="true"
+					/>
 					<link
 						href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap"
 						rel="stylesheet"
